237023d49478f16c0b410dd8dce5f790eb602fa6
extazystas <extazystas@gmail.com>
2020-01-30 01:01:08 +0300

    Working game setup is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Snake from './Snake';
 import Target from './Target';
+import useInterval from './useInterval';
 import './App.css';
+import skull from './skull.jpg';
 
 const getRandomTarget = () => {
   let min = 1;
@@ -12,11 +14,20 @@ const getRandomTarget = () => {
   return [x, y]
 }
 
+const initialState = {
+  snakePoints: [[0, 0], [2, 0]],
+  target: getRandomTarget,
+  direction: 'RIGHT',
+  speed: 200
+};
+
 function App() {
-  const [snakePoints, setSnakePoints] = useState([[0,0], [2,0]]);
-  const [target, generateTarget] = useState(getRandomTarget());
-  const [direction, setDirection] = useState('RIGHT');
-  const [speed, setSpeed] = useState(200);
+  const [snakePoints, setSnakePoints] = useState(initialState.snakePoints);
+  const [target, generateTarget] = useState(initialState.target());
+  const [direction, setDirection] = useState(initialState.direction);
+  const [speed, setSpeed] = useState(initialState.speed);
+  const [isGameOver, updateIsGameOver] = useState(false);
+  const [gameOverText, setGameOverText] = useState('Game Over.');
 
   const onKeyDown = (e) => {
     e = e || window.event;
@@ -41,6 +52,8 @@ function App() {
   }
 
   const moveSnake = () => {
+    if (isGameOver) return
+
     let dots = [...snakePoints];
     let head = dots[dots.length - 1];
 
@@ -67,19 +80,96 @@ function App() {
     setSnakePoints(dots);
   }
 
+  const handleOutside = () => {
+    let dots = [...snakePoints];
+    let head = dots[dots.length - 1];
+
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
+      onGameOver('outside');
+    }
+  }
+
+  const handleSelfEaten = () => {
+    let dots = [...snakePoints];
+    let head = dots[dots.length - 1];
+
+    dots.pop();
+    dots.forEach(dot => {
+      if (head[0] === dot[0] && head[1] === dot[1]) {
+        onGameOver('suicide');
+      }
+    })
+  }
+
+  const handleIncrease = () => {
+    let dots = [...snakePoints];
+    let head = dots[dots.length - 1];
+
+    if (head[0] === target[0] && head[1] === target[1]) {
+      generateTarget(getRandomTarget());
+      lvlUpSnake();
+      if (speed > 10) setSpeed(speed - 10);
+    }
+  }
+
+  const lvlUpSnake = () => {
+    let newSnake = [...snakePoints];
+
+    newSnake.unshift([]);
+    setSnakePoints(newSnake);
+  }
+
+  const onGameOver = (action) => {
+    let msg;
+
+    if (action === 'suicide') {
+      msg = "You ate yourself and committed suicide."
+    } else if(action === 'outside') {
+      msg = "You're squashed."
+    }
+
+    setGameOverText(`${msg} Snake length is ${snakePoints.length}.`);
+    updateIsGameOver(true)
+  }
+
+  const resetDashboard = () => {
+    setSnakePoints(initialState.snakePoints);
+    generateTarget(initialState.target());
+    setDirection(initialState.direction);
+    setSpeed(initialState.speed);
+    updateIsGameOver(false);
+  }
+
   useEffect(() => {
     window.addEventListener("keydown", onKeyDown)
-    // setInterval(moveSnake, 1000);
+    handleOutside();
+    handleSelfEaten();
+    handleIncrease();
+
     return () => {
       window.removeEventListener("keydown", onKeyDown)
     }
   })
 
+  useInterval(moveSnake, speed);
+
   return (
-    <div className='game-container'>
-      <Snake snakePoints={ snakePoints } />
-      <Target targetPoint={ target } />
-    </div>
+    <>
+      <div className='scoreboard'>{ snakePoints.length }</div>
+      <div className={ `game-container ${ isGameOver ? 'game-over-container' : '' }`}>
+      { isGameOver &&
+        <>
+          <div className='skull-image'><img src={ skull } alt="Deadman" /></div>
+          <div className='game-over-text'>{ gameOverText }</div>
+          <button className='reset-button' onClick={ () => resetDashboard() }>
+            Start Over!
+          </button>
+        </>
+      }
+        <Snake snakePoints={ snakePoints } />
+        <Target targetPoint={ target } />
+      </div>
+    </>
   );
 }
 
